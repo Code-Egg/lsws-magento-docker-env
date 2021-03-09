887853fbcba5e0dc779e46217978d01d946a21c5
Cold-Egg <eleu@litespeedtech.com>
2021-03-09 15:15:35 +0800

    123

--- bin/demosite.sh
+++ bin/demosite.sh
@@ -70,8 +70,8 @@ create_db(){
 
 set_phpmemory(){
     if [ "${1}" = 'magento' ]; then 
-        PHP_INI=$(docker-compose exec litespeed su -c "php -i | grep 'Loaded Configuration File' | cut -d' ' -f5 " | tr -d '\r')
-        PHP_MEMORY=$(docker-compose exec litespeed su -c "cat $PHP_INI | grep memory_limit" | tr -d '\r')
+        PHP_INI=$(docker-compose exec -T litespeed su -c "php -i | grep 'Loaded Configuration File' | cut -d' ' -f5 " | tr -d '\r')
+        PHP_MEMORY=$(docker-compose exec -T litespeed su -c "cat $PHP_INI | grep memory_limit" | tr -d '\r')
         docker-compose exec litespeed su -c "sed -i 's/^memory_limit.*/memory_limit = 755M/g' $PHP_INI"
         echo PHP_INI $PHP_INI
         echo PHP_MEMORY $PHP_MEMORY
@@ -80,7 +80,7 @@ set_phpmemory(){
 
 revert_phpmemory(){
     if [ "${1}" = 'magento' ]; then 
-        docker-compose exec litespeed /bin/bash -c "sed -i 's/^memory_limit.*/$PHP_MEMORY/g' $PHP_INI"
+        docker-compose exec -T litespeed /bin/bash -c "sed -i 's/^memory_limit.*/$PHP_MEMORY/g' $PHP_INI"
     fi    
 }    
 
